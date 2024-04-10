bd6f5104735db768c2c522a9ffcde02b0c280e1a
Diablo <staslepilov094@gmail.com>
2024-04-10 13:33:45 -0400

    Image

---
+++ React/src/Component/Images.tsx
@@ -0,0 +1,9 @@
+import style from '../App.module.css'
+export function Images(){
+    return(
+        <div className={style.test}>
+            <img src ='../images.jpeg' width={200}/>
+        
+        </div>
+    )
+}
